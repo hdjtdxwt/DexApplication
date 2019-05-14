06432d8a6493cb61b1d44a9b31b8c1363c5562c8
wangtao <wangtao@epsit.cn>
2019-05-15 00:28:36 +0800

    修改尝试调试

--- pluginapk/src/main/java/com/epsit/pluginapk/MainActivity.java
+++ pluginapk/src/main/java/com/epsit/pluginapk/MainActivity.java
@@ -10,34 +10,35 @@ import android.widget.Toast;
 
 import com.epsit.plugincore.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
     String TAG ="pluginapk-MainActivity";
     Button button;
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if(that == null){
+        if(that==null){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
             Log.e(TAG, "that == null  findViewById(R.id.third_btn)");
             button = findViewById(R.id.third_btn);
+            button.setOnClickListener(this);
         }else{
             Log.e(TAG, "that != null  findViewById(R.id.third_btn)");
-            button = that.findViewById(R.id.third_btn);
+            button = findViewById(R.id.third_btn);
+            button.setOnClickListener(this);
         }
 
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                /*if(that==null){
-                    Log.e(TAG, "that == null  Toast.makeText");
-                    Toast.makeText(getApplicationContext(), "我被点击了", Toast.LENGTH_SHORT).show();
-                }else{
-                    Log.e(TAG, "that != null  Toast.makeText");
-                    Toast.makeText(that, "我被点击了", Toast.LENGTH_SHORT).show();
-                }*/
-                Intent intent = new Intent(getApplicationContext(), TwoActivity.class);
-                startActivity(intent);
-            }
-        });
+
+    }
+    @Override
+    public void onClick(View v) {
+        if(that==null){
+            Log.e(TAG, "that == null  Toast.makeText");
+            Toast.makeText(getApplicationContext(), "我被点击了", Toast.LENGTH_SHORT).show();
+        }else{
+            Log.e(TAG, "that != null  Toast.makeText");
+            Toast.makeText(that, "我被点击了", Toast.LENGTH_SHORT).show();
+        }
+        Intent intent = new Intent(getApplicationContext(), TwoActivity.class);
+        startActivity(intent);
     }
 }
