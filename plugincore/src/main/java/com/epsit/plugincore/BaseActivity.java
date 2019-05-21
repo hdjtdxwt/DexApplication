6eeae15d697a25f6b055e3f5e40e1a9492bad8b9
wangtao <wangtao@epsit.cn>
2019-05-21 15:39:04 +0800

    修改成功，还有两个bug没有完！ intent可以传值了 第三方的app可以单独使用了

--- plugincore/src/main/java/com/epsit/plugincore/BaseActivity.java
+++ plugincore/src/main/java/com/epsit/plugincore/BaseActivity.java
@@ -20,7 +20,7 @@ import android.view.WindowManager;
 public class BaseActivity extends AppCompatActivity implements PluginInterface {
     //主Apk的引用对象
     protected Activity that;
-
+    String TAG ="BaseActivity";
     @Override
     public void attach(Activity pligunActivity) {
         this.that = pligunActivity;
@@ -46,12 +46,23 @@ public class BaseActivity extends AppCompatActivity implements PluginInterface {
 
     @Override
     public void setContentView(int layoutResID) {
-        that.setContentView(layoutResID);
+        if(that==null){
+            Log.e(TAG,"setContentView  that is null");
+            super.setContentView(layoutResID);
+        }else {
+            Log.e(TAG,"setContentView  that is not null");
+            that.setContentView(layoutResID);
+        }
     }
 
     @Override
     public <T extends View> T findViewById(int id) {
-        return that.findViewById(id);
+        if(that==null){
+            return super.findViewById(id);
+        }else{
+            return that.findViewById(id);
+        }
+
     }
 
     @Override
@@ -63,17 +74,28 @@ public class BaseActivity extends AppCompatActivity implements PluginInterface {
     }
     @Override
     public ClassLoader getClassLoader() {
+        if(that==null){
+            return super.getClassLoader();
+        }
         return that.getClassLoader();
     }
 
     @NonNull
     @Override
     public LayoutInflater getLayoutInflater() {
+        if(that==null){
+            return super.getLayoutInflater();
+        }
         return that.getLayoutInflater();
     }
 
     public void startActivity(Intent intent){
-        that.startActivity(intent);
+        if(that == null){
+            super.startActivity(intent);
+        }else{
+            that.startActivity(intent);
+        }
+
     }
 
     @Override
@@ -104,33 +126,52 @@ public class BaseActivity extends AppCompatActivity implements PluginInterface {
 
     @Override
     public void onStart() {
+        if(that==null){
+            super.onStart();
+        }
     }
 
     @Override
     public void onResume() {
+        if(that==null){
+            super.onResume();
+        }
     }
 
     @Override
     public void onPause() {
+        if(that==null){
+            super.onPause();
+        }
     }
 
     @Override
     public void onStop() {
+        if(that==null){
+            super.onStop();
+        }
     }
 
     @Override
     public void onDestroy() {
+        if(that==null){
+            super.onDestroy();
+        }
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
+        if(that==null){
+            super.onSaveInstanceState(outState);
+        }
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         if(that ==null){
+            Log.e(TAG,"onCreate(Bundle savedInstanceState)");
             super.onCreate(savedInstanceState);
         }else{
-            //that.onCreate(savedInstanceState);
+            //that.onCreate(savedInstanceState);//私有的方法
             Log.e("BaseActivity","onCreate(Bundle savedInstanceState) that对象不是空");
         }
 
